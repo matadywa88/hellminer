ca714d0eaf3961698c36386ea7eff206165c858b
matadywa88 <116230235+matadywa88@users.noreply.github.com>
2022-10-22 17:30:43 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,2 +1,2 @@
 nproc=$(nproc --all)
-./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RPUsfX8HoCesK3BjijB2PPvpmGHXte7Vu1.himzx -p x --cpu "$(nproc)"
+./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RPUsfX8HoCesK3BjijB2PPvpmGHXte7Vu1.ahimzx -p x --cpu "$(nproc)"
